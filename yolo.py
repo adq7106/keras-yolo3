abcaf3cda9f5109341790cbb2ece6109a34be2a9
character-s <45820854+character-s@users.noreply.github.com>
2018-12-12 21:44:10 +0900

    Add files via upload

--- yolo.py
+++ yolo.py
@@ -209,4 +209,18 @@ def detect_video(yolo, video_path, output_path=""):
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     yolo.close_session()
-
+def detect_img(yolo):
+    while True:
+        img = input('Input image filename:')
+        try:
+            image = Image.open(img)
+        except:
+            print('Open Error! Try again!')
+            continue
+        else:
+            r_image = yolo.detect_image(image)
+            r_image.show()
+            r_image.save("result.jpg")
+    yolo.close_session()
+if __name__ == '__main__':
+    detect_img(YOLO())
